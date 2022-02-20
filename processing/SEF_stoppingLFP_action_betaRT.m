bc1dee7fa8ec34461182097d1322dc26abba3ab3
Steven Errington <steven.errington@outlook.com>
2022-02-19 20:56:37 -0600

    Looking at the relationship between beta and RT on no-stop trials

--- processing/SEF_stoppingLFP_action_betaRT.m
+++ processing/SEF_stoppingLFP_action_betaRT.m
@@ -10,7 +10,7 @@ parfor lfpIdx = 1:length(corticalLFPcontacts.all)
     
     % Get session name (to load in relevant file)
     sessionName = FileNames{session};
-    fprintf('Analysing LFP number %i of 509. \n',lfp);
+    fprintf('Analysing LFP number %i of 509. \n',lfpIdx);
     
     % Load in beta output data for session
     loadname = ['betaBurst\target\lfp_session' int2str(session) '_' sessionLFPmap.channelNames{lfp} '_betaOutput_target'];
@@ -32,11 +32,11 @@ parfor lfpIdx = 1:length(corticalLFPcontacts.all)
     for trlIdx = 1:length(trialGO)
         % Get whether a burst occured within the given time window
         % post-saccade (no burst = 0, burst = 1)
-        window = [-400 -200]
+        window = [0 100]
         burstFlag(trlIdx,1) = double(sum((betaOutput.burstData.burstTime{trialGO(trlIdx)} < window(2) &...
             betaOutput.burstData.burstTime{trialGO(trlIdx)} > window(1)) == 1) > 0);
-        burstFlag(trlIdx,2) = double(sum((betaOutput.burstData.burstTime{trialGO(trlIdx)} < 200 &...
-            betaOutput.burstData.burstTime{trialGO(trlIdx)} > 0) == 1) > 0);       
+        burstFlag(trlIdx,2) = double(sum((betaOutput.burstData.burstTime{trialGO(trlIdx)} < sessionRT(trialGO(trlIdx)) &...
+            betaOutput.burstData.burstTime{trialGO(trlIdx)} > sessionRT(trialGO(trlIdx)) - 200) == 1) > 0);
         % Get the change in RT from the NC to the GO trial
         % minus values represent slowing, positive values represent
         % speeding.
@@ -48,111 +48,69 @@ parfor lfpIdx = 1:length(corticalLFPcontacts.all)
     
 end
 
-%% Model 1: Logisitc regression
-%  Here, I looked at fitting a logistic regression see the association between a
-%  burst (0 or 1) in the error trial, and RT adaption in the following
-%  trial
 
-% Initialise array
-rtSlow_model = [];
-
-% For each contact
-for lfpIdx = 1:length(corticalLFPcontacts.all)
-    fprintf('Analysing LFP number %i of 509. \n',lfpIdx);
-    % Get the burst and RT adaptation for the contact
-    burstFlag = logTable{lfpIdx}(:,1); RTslowing = logTable{lfpIdx}(:,2);
-    % Convert into a table for the regression function
-    regressionTable = table(burstFlag, RTslowing);
-    % Run the regression
-    model = fitglm(regressionTable,'burstFlag ~ RTslowing','link','logit','Distribution','binomial');
-    % Save the regression output (R^2 value and p-value in rtSlow_model,
-    % and beta value in rtSlow_betaValue
-    rtSlow_model(lfpIdx,:) = [model.Rsquared.Ordinary, model.Coefficients.pValue(2)];
-    rtSlow_betaValue(lfpIdx,:) = model.Coefficients.Estimate';
-end
-
-% After running this for all contacts, find those with a sig p-value
-% (p< 1 / nContacts - multiple comparisons)
-% Also tried this at p = 0.01
-pSigLevel = 0.05/length(corticalLFPcontacts.all);
-sigSlow = find(rtSlow_model(:,2) < pSigLevel);
-
-% For each significant contact, I then printed a figure to check over
-for idx = 1:length(sigSlow)
-    lfpIdx = sigSlow(idx);
-    % Create figure
-    figure; hold on
-    % Plot the RT adapation and burst values
-    scatter(logTable{lfpIdx}(:,2),logTable{lfpIdx}(:,1))
+%%
+clear pBurst_baseline_quantile pBurst_target_quantile
+for sessionIdx = 1:29
+    sessionLFPch = find(corticalLFPmap.session == sessionIdx);
     
-    % Derive values from regression equation, and plot that.
-    rtSlowRange = linspace(-200,10,200);
-    beta = rtSlow_betaValue(lfpIdx,:);
-    plot(rtSlowRange, 1./(1+exp(-(beta(1)+beta(2)*rtSlowRange))))
+    RT_lfp = RT_beta_assoc{sessionLFPch(1)}(:,3);
+    RT_lfp = RT_lfp(RT_lfp > 100);
+    RT_quantiles = quantile(RT_lfp,[0.0 0.2 0.4 0.6 0.8 1.0]);
+    
+    burst_baseline_sessionLFP = []; burst_target_sessionLFP = [];
+    burst_baseline_session = []; burst_target_session = [];
+    
+    for sessionLFPidx = 1:length(sessionLFPch)
+        lfpIdx = sessionLFPch(sessionLFPidx);
+        burst_baseline_sessionLFP(:,sessionLFPidx) = RT_beta_assoc{lfpIdx}(:,1);
+        burst_target_sessionLFP(:,sessionLFPidx) = RT_beta_assoc{lfpIdx}(:,2);
+    end
+    
+    burst_baseline_session = mean(burst_baseline_sessionLFP,2);
+    burst_target_session = mean(burst_target_sessionLFP,2);
+    
+    
+    for quantileIdx = 1:length(RT_quantiles)-1
+        quantileTrials = [];
+        quantileTrials = find(RT_lfp >= RT_quantiles(quantileIdx) & RT_lfp <= RT_quantiles(quantileIdx+1));
+        
+        pBurst_baseline_quantile(sessionIdx, quantileIdx) = mean(burst_baseline_session(quantileTrials));
+        pBurst_target_quantile(sessionIdx, quantileIdx) = mean(burst_target_session(quantileTrials));
+        RT_quantile_out(sessionIdx, quantileIdx) = RT_quantiles(quantileIdx);
+    end
     
 end
 
-% I don't think this works particularly well, so I run with option 2.
 
-%% Model 2: Independent groups comparison between error trials with/without a burst
-%  For each contact
-for lfpIdx = 1:length(corticalLFPcontacts.all)
-    clear lfpBurstRTtable 
-    % Get the burst and RT adaptation for the contact
-    lfpBurstRTtable = logTable{(lfpIdx)};
-    
-    % Run a t-test on differences in RT adaption between trials with bursts
-    % and those without
-    [h, ~, ~, tstat_out] = ttest2(...
-        lfpBurstRTtable(lfpBurstRTtable(:,1) == 1,2),... % RT on go following error trials with burst
-        lfpBurstRTtable(lfpBurstRTtable(:,1) == 0,2),... % RT on go following error trials without burst
-        'alpha',pSigLevel);... % Setting alpha level at 1/509 for multiple comparisons
-    
-    % Get test output
-    rt_ttest(lfpIdx,1) = h; % Significant flag (1 = significant difference at p = 1/509)
-    
-    rt_ttest(lfpIdx,2) = nanmean... % mean difference between groups
-        (lfpBurstRTtable(lfpBurstRTtable(:,1) == 1,2))-... %  RT on go following error trials with burst
-        nanmean(lfpBurstRTtable(lfpBurstRTtable(:,1) == 0,2)); % RT on go following error trials without burst
-    % Here, -ve represents greater slowing with burst
-    
-    rt_ttest(lfpIdx,3) = tstat_out.tstat; % Get the t-statistic
+RT_all = reshape(RT_quantile_out,[],1);
+pBurst_baseline_all = reshape(pBurst_baseline_quantile,[],1);
+pBurst_target_all = reshape(pBurst_target_quantile,[],1);
 
-    
+sessionLabel = {};
+for sessionIdx = 1:29
+    sessionLabel = [sessionLabel; repmat(executiveBeh.nhpSessions.monkeyNameLabel(sessionIdx),5,1)];
 end
 
-%% Plot figures
+%% Generate figure
+figure('Renderer', 'painters', 'Position', [100 100 350 300]);
+g=gramm('x',RT_all,'y',pBurst_target_all);
+g.geom_point(); g.stat_glm('fullrange',true,'disp_fit',true)
+g.axe_property('XLim',[50 450]);g.axe_property('YLim',[0 0.45])
+g.draw()
 
-% Histogram of significant and non-significant differences in RT-adaptation between
-% burst/no-burst
-rtAdjustment_histogram(1,1)= gramm('x',rt_ttest(:,2),...% Here I am plotting the difference in RT adaptation between trials with a burst, and trials without. -ve values = greater slowing with burst
-    'color',rt_ttest(:,1));
-rtAdjustment_histogram(1,1).stat_bin('edges',-100:20:100,'dodge',0,'geom','bar');
-figure('Renderer', 'painters', 'Position', [100 100 300 250]);
-rtAdjustment_histogram.draw();
-
-% Pie chart showing significant direction differences (non-sig,
-% positive RT adaptation with burst, negative RT adaptation with burst)
-pContactType = [sum(rt_ttest(:,1) == 0),... % Non-significant
-    sum(rt_ttest(:,1) == 1 & rt_ttest(:,2) > 0),... % Greater slowing w/o burst
-    sum(rt_ttest(:,1) == 1 & rt_ttest(:,2) < 0)];   % Greater slowing w/ burst
-figure('Renderer', 'painters', 'Position', [100 100 300 250]);
-explode = [1 1 1];
-pie(pContactType,explode)
+figure('Renderer', 'painters', 'Position', [100 100 350 300]);
+g=gramm('x',RT_all,'y',pBurst_target_all,'color',sessionLabel);
+g.geom_point(); g.stat_glm('fullrange',true,'disp_fit',true)
+g.axe_property('XLim',[50 450]);g.axe_property('YLim',[0 0.45])
+g.draw()
 
 
-% Bar chart showing differences in burst/no-burst RT adaptation for an example contact
-clear rtAdjustment_a
-% - First, find the lfp with the greatest difference
-posIdx = find(rt_ttest(:,1) == 1);
-[~,closestIndex] = sort(abs(rt_ttest(posIdx,2)-max(rt_ttest(posIdx,2))));
-lfpIdx = posIdx(closestIndex == 1);
+%% Generate figure
+figure('Renderer', 'painters', 'Position', [100 100 350 300]);
+g=gramm('x',RT_beta_assoc{10}(:,3));
+g.stat_density();
+g.draw()
 
-% - Then plot this example contact
-rtAdjustment_a(1,1)= gramm('x',logTable{(lfpIdx)}(:,1),...
-    'y',logTable{(lfpIdx)}(:,2));
-rtAdjustment_a(1,1).stat_summary('geom',{'bar','black_errorbar'});
-rtAdjustment_a(1,1).axe_property('XLim',[-0.5 1.5]);
-rtAdjustment_a(1,1).axe_property('YLim',[-200 200]);
-figure('Renderer', 'painters', 'Position', [100 100 300 250]);
-rtAdjustment_a.draw();
+edit examples
+
